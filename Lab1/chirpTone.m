f10cafc270dc3106ba8e6bfbe354f8daaf1ace10
Patricia Marques <mpatricia2002@gmail.com>
2023-03-03 11:42:29 +0000

    lab sexta

--- Lab1/chirpTone.m
+++ Lab1/chirpTone.m
@@ -2,7 +2,7 @@ function x = chirpTone(T,f1,f2,fs)
     ts = 1/fs; %time step for time array
     time = 0:ts:T; %time array
     %instead of a cosine, we introduce the phase -pi/2 for a sine
-    x = chirp(time, f1, T, f2,"linear",-pi/2);
-    %f = f1:(f2-f1)/(length(time)-1):f2
-    %x = sin(2*pi*f*time);
+    f = f1:(f2-f1)/(length(time)-1):f2;
+    x = sin(2*pi*f.*time);
+    %x = chirp(time, f1, T, f2,"linear",-pi/2);
 end
